3d6786e77dde07fa2bb257a92e0fc230c903eab2
Stefan Fodor <stefan@unserialized.dk>
2019-12-12 21:12:43 +0200

    Day 11, Part 2

--- Day 11/day 11.go
+++ Day 11/day 11.go
@@ -10,9 +10,7 @@ type Point struct {
 	y int
 }
 
-func getPanelCount() int {
-
-	var panelColor map[Point]int = make(map[Point]int)
+func paintPanel(panel map[Point]int) map[Point]int {
 
 	var orientation int = 0
 	var currentPosition Point = Point{
@@ -24,9 +22,9 @@ func getPanelCount() int {
 	program.Execute()
 
 	for !program.Halted {
-		program.SendInput( panelColor[currentPosition] )
+		program.SendInput( panel[currentPosition] )
 
-		panelColor[currentPosition] = program.Output[ len(program.Output) - 2 ]
+		panel[currentPosition] = program.Output[ len(program.Output) - 2 ]
 
 		// turn right or left
 		if program.Output[ len(program.Output) - 1 ] == 1 {
@@ -47,10 +45,65 @@ func getPanelCount() int {
 		}
 	}
 
-	return len(panelColor)
+	return panel
+}
+
+func drawPanel(panel map[Point]int)  {
+	fmt.Printf("Your panel here, Panel Count: %d \n", len(panel))
+
+	var start = Point{
+		x: 9999,
+		y: 9999,
+	}
+
+	var end = Point{
+		x: -9999,
+		y: -9999,
+	}
+
+	for point,_ := range panel {
+		if point.x < start.x {
+			start.x = point.x
+		}
+		if point.y < start.y {
+			start.y = point.y
+		}
+
+		if point.x > end.x {
+			end.x = point.x
+		}
+		if point.y > end.y {
+			end.y = point.y
+		}
+	}
+
+	for i := start.y; i<=end.y; i++ {
+		for j := start.x; j<=end.x; j++ {
+			var thisPoint = Point{
+				x: j,
+				y: i,
+			}
+			if panel[thisPoint] == 1 {
+				fmt.Print("■")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\n")
+	}
+
+
 }
 
 func main() {
-	var panelCount = getPanelCount()
-	fmt.Printf("Panel Count: %d", panelCount)
+
+	//var panel map[Point]int = make(map[Point]int)
+	//panel = paintPanel(panel)
+	//fmt.Printf("Panel Count: %d", len(panel))
+
+	var panel map[Point]int = make(map[Point]int)
+	panel[Point{x: 0, y: 0}] = 1
+	panel = paintPanel(panel)
+	drawPanel(panel)
+
 }
